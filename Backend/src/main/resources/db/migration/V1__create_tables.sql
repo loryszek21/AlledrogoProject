31f0122ea232dee6c64b7dad1e97a1cf005519d2
dawid <dawidlorych20@gmail.com>
2024-10-14 22:38:19 +0200

    flyWay init

---
+++ Backend/src/main/resources/db/migration/V1__create_tables.sql
@@ -0,0 +1,9 @@
+CREATE TABLE Users (
+                       user_id SERIAL,
+                       username VARCHAR(50),
+                       password TEXT,
+                       first_name VARCHAR(50),
+                       last_name VARCHAR(50),
+                       email VARCHAR(100)
+
+);
---
+++ Backend/src/main/resources/db/migration/V2__insert_initial_user.sql
@@ -0,0 +1,1 @@
+INSERT INTO Users(username, password, first_name, last_name, email) VALUES ('User1', 'admin', 'UserName', 'UserLastName', 'user@email.com' );
