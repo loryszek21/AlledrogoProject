9b44e945ef44abeb21f72e8a433826391ce867f4
Kubekxd5 <jakub.kozminski@vp.pl>
2024-12-02 17:38:57 +0100

    Security, Login and Registration controllers...

---
+++ Backend/src/main/java/com/example/backend/controller/RegistrationController.java
@@ -0,0 +1,23 @@
+package com.example.backend.controller;
+
+import com.example.backend.model.MyAppUser;
+import com.example.backend.model.MyAppUserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RegistrationController {
+    @Autowired
+    private MyAppUserRepository myAppUserRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @PostMapping(value="req/signup", consumes = "application/json")
+    public MyAppUser createUser(@RequestBody MyAppUser user){
+        user.setPassword(passwordEncoder.toString());
+        return myAppUserRepository.save(user);
+    }
+}
