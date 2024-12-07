24a9cec232a8db75eaad183fddcc3f8255e5bf89
dawid <dawidlorych20@gmail.com>
2024-12-07 22:07:06 +0100

    little bugFix in input type

--- frontend/src/app/(site)/components/InputField/InputField.js
+++ frontend/src/app/(site)/components/InputField/InputField.js
@@ -1,12 +1,12 @@
 "use client";
 import React from "react";
 
-function InputField({ id, type = "text", placeholder, value, onChange }) {
+function InputField({ id, type , placeholder, value, onChange }) {
   return (
     <div className="relative md:md-6   " data-twe-input-wrapper-init>
       <input
-        type="{type}"
         id="{id}"
+        type={type}
         value={value}
         onChange={onChange}
         className="peer block min-h-[auto] w-full rounded border-0 bg-transparent px-3 py-[0.32rem] leading-[1.6] outline-none transition-all duration-200 ease-linear focus:placeholder:opacity-100 peer-focus:text-primary data-[twe-input-state-active]:placeholder:opacity-100 motion-reduce:transition-none dark:text-white dark:placeholder:text-neutral-300 dark:autofill:shadow-autofill dark:peer-focus:text-primary [&:not([data-twe-input-placeholder-active])]:placeholder:opacity-0"
